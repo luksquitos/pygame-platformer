caa864f8dbb7e8d4aa1e76c9b58f75783e18b1d3
lucas <lucasmartinscaet@hotmail.com>
2025-04-14 16:20:09 -0300

    refact: check collisions pass returns a list of objects collided

--- objects.py
+++ objects.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import Tuple, List, Optional
 from abc import ABC
-
+from random import randint as r
 import pygame as pg
 
 from core.images import load_image
@@ -19,15 +19,12 @@ class Object(ABC):
         # size: Tuple[int, int] | None = None
     ) -> None:
         
-        print(self.__class__)
-        
         if not self.frame_path and not surface:
             raise ValueError("Must provide 'frame_path' or existing'surface'")
         
         # assert self.slug, f"slug not provided for {self.__class__.__name__}"
         
         self.frame = surface or load_image(self.frame_path, size)
-        print(self.frame)
         
         #print(self.frame, " para ", self.__class__.__name__)
             
@@ -36,10 +33,14 @@ class Object(ABC):
     
     def update(self, screen: pg.Surface, objs_in_memory: List[Object]) -> None:
         self.render(screen)
-        self.check_collisions(objs_in_memory)
-        
-        if self.check_collisions(objs_in_memory):
-            print(self.__class__, "Colidiu com ", self.check_collisions(objs_in_memory))
+        self.collisions = self.check_collisions(objs_in_memory)
+        self.perform_collision_action()
+        # print(
+        #     f"{self.__class__}\n",
+        #     f"top: {self.rect.top}\n",
+        #     f"bottom: {self.rect.bottom}",
+        # )
+        # print("=========" * 10)
         
         
     @property
@@ -49,14 +50,25 @@ class Object(ABC):
     def render(self, screen: pg.Surface) -> None:
         screen.blit(self.frame, (self.x_pos, self.y_pos))
         
-    def check_collisions(self, objs_in_memory: List[Object]) -> None:
+    def check_collisions(self, objs_in_memory: List[Object]) -> List[Object]:
         objs_copy = objs_in_memory.copy()
         objs_copy.remove(self) # objects can't collide with himself
+        collisions = []
+        
         for obj in objs_copy:
             if not self.rect.colliderect(obj.rect):
                 continue
             
-            self.perform_collision_action(obj)
+            collisions.append(obj)
+        
+        return collisions
+    
+    def perform_collision_action(self) -> None:
+        # print(
+        #     f"{self.__class__.__name__} doesn't have a action for {obj.__class__.__name__}"
+        # )
+        
+        return None
     
     @property
     def can_delete(self) -> bool:
@@ -69,13 +81,6 @@ class Object(ABC):
     @can_delete.setter
     def can_delete(self, value: bool) -> None:
         self._can_delete = value
-    
-    def perform_collision_action(self, obj) -> None:
-        # print(
-        #     f"{self.__class__.__name__} doesn't have a action for {obj.__class__.__name__}"
-        # )
-        
-        return None
 
 
 class MovingObject(Object):
